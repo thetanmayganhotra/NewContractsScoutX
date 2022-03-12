b0f690b98506fde92b3d44679e065fb75e7b1056
thetanmayganhotra <tanmay.ganhotra@gmail.com>
2022-03-12 10:27:58 +0530

    fpmmfactory

--- FPMM.sol
+++ FPMM.sol
@@ -1,17 +1,11 @@
-// SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
 
+
+pragma solidity ^0.8.0;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ConditionalTokens.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC1155Receiver } from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
-
-
-
 library CeilDiv {
     // calculates ceil(x/y)
     function ceildiv(uint x, uint y) internal pure returns (uint) {
@@ -19,13 +13,10 @@ library CeilDiv {
         return x / y;
     }
 }
-
 //todo: shortfall, volatility as parameters (modifiable)
 //todo: add spread which is a function shortfall and volatility
 //todo: add lower bounds to the price
-
-
-contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
+contract FixedProductMarketMaker is ERC20, ERC1155Receiver {
     event FPMMFundingAdded(
         address indexed funder,
         uint[] amountsAdded,
@@ -51,8 +42,6 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
         uint indexed outcomeIndex,
         uint outcomeTokensSold
     );
-
-    
     event FPMMCreated(
         address indexed creator,
         string tokenName,
@@ -62,110 +51,89 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
         bytes32 conditionIds,
         uint fee
     );
-
-    
-
+    event TransferredOwner(
+        address indexed owner,
+        address previousOwner
+    );
     using CeilDiv for uint;
-
-    uint constant ONE = 10**18; // 1% == 0.01 == 1016 == 1016 / ONE = 10*-2 == 0.01
-
-    
+    uint constant ONE = 10**18; // 1% == 0.01 == 10**16 == 10**16 / ONE = 10**-2 == 0.01
+    address private owner;
+    address private oracle;
     ConditionalTokens private conditionalTokens;
     IERC20 private collateralToken;
-    
-
     uint private fee;
     uint internal feePoolWeight;
-
     bytes32 private conditionId;
-
-    address public oracle;
-
-    bytes32 public questionId;
+    bytes32 private questionId;
     
     bytes32[] private collectionIds;
+    uint[] private positionIds;
     uint private longPositionId;
     uint private shortPositionId;
-
     uint constant numPositions = 2;
-
-    mapping(bytes32 => bytes32) questionIdtoConditionId; 
-
     mapping (address => uint256) withdrawnFees;
     uint internal totalWithdrawnFees;
-
-    mapping(bytes32 => mapping(uint => uint)) public QidTokenBalance;
-
     constructor(
-        string memory name, string memory symbol, // name symbol playerId
+        string memory name,
+        string memory symbol,
         address _conditionalTokensAddr,
         address _collateralTokenAddr,
-        uint _fee,
-        address _oracle,
         bytes32 _questionId,
-        address _owner
-    ) ERC20(name, symbol){
+        address _oracle,
+        uint _fee
+    ) ERC20(name, symbol) {
         fee = _fee;
-        
-        oracle = _oracle;
         questionId = _questionId;
-        collateralToken = IERC20(_collateralTokenAddr);
+        oracle = _oracle;
+        collateralToken = ERC20(_collateralTokenAddr);
         conditionalTokens = ConditionalTokens(_conditionalTokensAddr);
-
-        address owner = _owner;
-       
-
-        
-
-
-       
-       
-        
-        conditionId = conditionalTokens.getConditionId(oracle,questionId,2);
-
-        questionIdtoConditionId[questionId] = conditionId;
-
+        conditionalTokens.prepareCondition(oracle, questionId, 2);
+        conditionId = conditionalTokens.getConditionId(oracle, questionId, 2);
         collectionIds = new bytes32[](2);
         collectionIds[0] = conditionalTokens.getCollectionId(bytes32(0), conditionId, 1);
         collectionIds[1] = conditionalTokens.getCollectionId(bytes32(0), conditionId, 2);
-
-
-
         longPositionId = conditionalTokens.getPositionId(collateralToken, collectionIds[0]);
         shortPositionId = conditionalTokens.getPositionId(collateralToken, collectionIds[1]);
-
-        
-
+        positionIds = new uint[](2);
+        positionIds[0] = longPositionId;
+        positionIds[1] = shortPositionId;
+        owner = msg.sender;
         emit FPMMCreated(
-            msg.sender, name, symbol, _conditionalTokensAddr, _collateralTokenAddr, conditionId, _fee
+            msg.sender, name, symbol, _conditionalTokensAddr, _collateralTokenAddr, _questionId, _fee
         );
     }
-
-    
-    function getPositionIds() public view returns (uint[] memory) {
-        uint[] memory arr = new uint[](2);
-        arr[0] = longPositionId;
-        arr[1] = shortPositionId;
-        return arr;
+    modifier onlyOwner() {
+        require(isOwner(msg.sender), "Restricted to Owner.");
+        _;
     }
-
-
+    function transferOwner(address newOwner) public onlyOwner {
+        owner = newOwner;
+        emit TransferredOwner(newOwner, address(this));
+    }
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+    function getBalancesFor(address target) public view returns (uint[] memory) {
+        address[] memory targets = new address[](2);
+        targets[0] = target;
+        targets[1] = target;
+        return conditionalTokens.balanceOfBatch(targets, positionIds);
+    }
+    function getPositionIds() public view returns (uint[] memory) {
+        return positionIds;
+    }
+    function isOwner(address sender) public view returns (bool) {
+        return sender == owner;
+    }
     function getFee() public view returns (uint) {
         return fee;
     }
-
     function setFees(uint newFees) public onlyOwner {
         fee = newFees;
     }
-
     function getPoolBalances() public view returns (uint[] memory) {
-        address[] memory thises = new address[](2);
-        for(uint i = 0; i < thises.length; i++) {
-            thises[i] = address(this);
-        }
-        return conditionalTokens.balanceOfBatch(thises, getPositionIds());
+        return getBalancesFor(address(this));
     }
-
     function generateBasicPartition(uint outcomeSlotCount)
         public
         pure
@@ -176,7 +144,6 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
             partition[i] = 1 << i;
         }
     }
-
     function splitPositionThroughAllConditions(uint amount)
         private
     {
@@ -184,10 +151,7 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
         partition[0] = 1;
         partition[1] = 2;
         conditionalTokens.splitPosition(collateralToken, bytes32(0), conditionId, partition, amount);
-        QidTokenBalance[questionId][0] = amount;
-        QidTokenBalance[questionId][1] = amount;
     }
-
     function mergePositionsThroughAllConditions(uint amount)
         private
     {
@@ -198,16 +162,13 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
             conditionalTokens.mergePositions(collateralToken, collectionIds[j], conditionId, partition, amount);
         }
     }
-
     function collectedFees() external view returns (uint) {
         return feePoolWeight - totalWithdrawnFees;
     }
-
     function feesWithdrawableBy(address account) public view returns (uint) {
         uint rawAmount = feePoolWeight * (balanceOf(account)) / totalSupply();
         return rawAmount - withdrawnFees[account];
     }
-
     function withdrawFees(address account) public {
         uint rawAmount = feePoolWeight * (balanceOf(account)) / totalSupply();
         uint withdrawableAmount = rawAmount - (withdrawnFees[account]);
@@ -217,17 +178,14 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
             require(collateralToken.transfer(account, withdrawableAmount), "withdrawal transfer failed");
         }
     }
-
     function _beforeTokenTransfer(address from, address to, uint256 amount) override internal {
         if (from != address(0)) {
             withdrawFees(from);
         }
-
         uint totalSupply = totalSupply();
         uint withdrawnFeesTransfer = totalSupply == 0 ?
             amount :
             feePoolWeight * (amount) / totalSupply;
-
         if (from != address(0)) {
             withdrawnFees[from] = withdrawnFees[from] - withdrawnFeesTransfer;
             totalWithdrawnFees = totalWithdrawnFees - withdrawnFeesTransfer;
@@ -241,12 +199,10 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
             feePoolWeight = feePoolWeight - withdrawnFeesTransfer;
         }
     }
-
     function addFunding(uint addedFunds, uint[] calldata distributionHint)
         external
     {
         require(addedFunds > 0, "funding must be non-zero");
-
         uint[] memory sendBackAmounts = new uint[](numPositions);
         uint poolShareSupply = totalSupply();
         uint mintAmount;
@@ -259,12 +215,10 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
                 if(poolWeight < balance)
                     poolWeight = balance;
             }
-
             for(uint i = 0; i < poolBalances.length; i++) {
                 uint remaining = addedFunds * (poolBalances[i]) / poolWeight;
                 sendBackAmounts[i] = addedFunds - (remaining);
             }
-
             mintAmount = addedFunds * (poolShareSupply) / poolWeight;
         } else {
             if(distributionHint.length > 0) {
@@ -275,64 +229,48 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
                     if(maxHint < hint)
                         maxHint = hint;
                 }
-
                 for(uint i = 0; i < distributionHint.length; i++) {
                     uint remaining = addedFunds * (distributionHint[i]) / maxHint;
                     require(remaining > 0, "must hint a valid distribution");
                     sendBackAmounts[i] = addedFunds - (remaining);
                 }
             }
-
             mintAmount = addedFunds;
         }
-
         require(collateralToken.transferFrom(msg.sender, address(this), addedFunds), "funding transfer failed");
         require(collateralToken.approve(address(conditionalTokens), addedFunds), "approval for splits failed");
         splitPositionThroughAllConditions(addedFunds);
-
         _mint(msg.sender, mintAmount);
-
         conditionalTokens.safeBatchTransferFrom(address(this), msg.sender, getPositionIds(), sendBackAmounts, "");
-
         // transform sendBackAmounts to array of amounts added
         for (uint i = 0; i < sendBackAmounts.length; i++) {
             sendBackAmounts[i] = addedFunds - sendBackAmounts[i];
         }
-
         emit FPMMFundingAdded(msg.sender, sendBackAmounts, mintAmount);
     }
-
     function removeFunding(uint sharesToBurn)
         external
     {
         uint[] memory poolBalances = getPoolBalances();
-
         uint[] memory sendAmounts = new uint[](poolBalances.length);
-
         uint poolShareSupply = totalSupply();
         for(uint i = 0; i < poolBalances.length; i++) {
             sendAmounts[i] = poolBalances[i] * (sharesToBurn) / poolShareSupply;
         }
-
         uint collateralRemovedFromFeePool = collateralToken.balanceOf(address(this));
-
         _burn(msg.sender, sharesToBurn);
         collateralRemovedFromFeePool = collateralRemovedFromFeePool - collateralToken.balanceOf(address(this));
-
         conditionalTokens.safeBatchTransferFrom(address(this), msg.sender, getPositionIds(), sendAmounts, "");
-
         emit FPMMFundingRemoved(msg.sender, sendAmounts, collateralRemovedFromFeePool, sharesToBurn);
     }
-
     function onERC1155Received(
         address operator,
         address from,
         uint256 id,
         uint256 value,
         bytes calldata data
-    )
+    )   override
         external
-        override
         returns (bytes4)
     {
         if (operator == address(this)) {
@@ -340,7 +278,6 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
         }
         return 0x0;
     }
-
     function onERC1155BatchReceived(
         address operator,
         address from,
@@ -356,25 +293,18 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
         }
         return 0x0;
     }
-
     function getPrices() public view returns (uint[] memory) {
         uint[] memory prices = new uint[](2);
         uint[] memory poolBalances = getPoolBalances();
         require(poolBalances.length == 2, "incorrect number of balances in pool");
-
         uint x1 = poolBalances[0];
         uint x2 = poolBalances[1];
-
         prices[0] = ONE * x2 / (x1 + x2);
         prices[1] = ONE * x1 / (x1 + x2);
-
         return prices;
     }
-
     function calcBuyAmount(uint investmentAmount, uint outcomeIndex) public view returns (uint) {
         require(outcomeIndex < numPositions, "invalid outcome index");
-        
-
         uint[] memory poolBalances = getPoolBalances();
         uint investmentAmountMinusFees = investmentAmount - (investmentAmount * (fee) / ONE);
         uint buyTokenPoolBalance = poolBalances[outcomeIndex];
@@ -385,77 +315,55 @@ contract FixedProductMarketMaker is ERC20, ERC1155Receiver, Ownable{
                 endingOutcomeBalance = endingOutcomeBalance * (poolBalance).ceildiv(poolBalance + investmentAmountMinusFees);
             }
         }
-
         require(endingOutcomeBalance > 0, "must have non-zero balances");
-
         return (buyTokenPoolBalance + investmentAmountMinusFees) - (endingOutcomeBalance.ceildiv(ONE));
     }
-
     function calcSellAmount(uint returnAmount, uint outcomeIndex) public view returns (uint outcomeTokenSellAmount) {
         require(outcomeIndex < numPositions, "invalid outcome index");
-
-        uint[] memory poolBalances = getPoolBalances();//100 
-        uint returnAmountPlusFees = (returnAmount * ONE) / (ONE - fee); //10^19 / (10^18 - 10) = 10
-        uint sellTokenPoolBalance = poolBalances[outcomeIndex]; //100
-        uint endingOutcomeBalance = sellTokenPoolBalance * ONE;// 100* 10^18 = 10^20
+        uint[] memory poolBalances = getPoolBalances();
+        uint returnAmountPlusFees = (returnAmount * ONE) / (ONE - fee);
+        uint sellTokenPoolBalance = poolBalances[outcomeIndex];
+        uint endingOutcomeBalance = sellTokenPoolBalance * ONE;
         for(uint i = 0; i < poolBalances.length; i++) {
             if(i != outcomeIndex) {
-                uint poolBalance = poolBalances[i];//100
-                endingOutcomeBalance = endingOutcomeBalance * (poolBalance).ceildiv(poolBalance - returnAmountPlusFees); 
-                //10^20 * 100.ceildiv(100 - 10) => a.cd(b) => -( a//-b ) = 10^20 * (-(100//-90)) = 10^20 * 1 = 10^20
+                uint poolBalance = poolBalances[i];
+                endingOutcomeBalance = endingOutcomeBalance * (poolBalance).ceildiv(poolBalance - returnAmountPlusFees);
             }
         }
         require(endingOutcomeBalance > 0, "must have non-zero balances");
-
-        return returnAmountPlusFees + (endingOutcomeBalance.ceildiv(ONE)) - sellTokenPoolBalance; //  10
+        return returnAmountPlusFees + (endingOutcomeBalance.ceildiv(ONE)) - sellTokenPoolBalance;
     }
-
     function buy(uint investmentAmount, uint outcomeIndex, uint minOutcomeTokensToBuy) external {
         uint outcomeTokensToBuy = calcBuyAmount(investmentAmount, outcomeIndex);
         require(outcomeTokensToBuy >= minOutcomeTokensToBuy, "minimum buy amount not reached");
-
         require(collateralToken.transferFrom(msg.sender, address(this), investmentAmount), "cost transfer failed");
-
         uint feeAmount = investmentAmount * fee / ONE;
         feePoolWeight = feePoolWeight + feeAmount;
         uint investmentAmountMinusFees = investmentAmount - feeAmount;
         require(collateralToken.approve(address(conditionalTokens), investmentAmountMinusFees), "approval for splits failed");
         splitPositionThroughAllConditions(investmentAmountMinusFees);
-
         conditionalTokens.safeTransferFrom(address(this), msg.sender, getPositionIds()[outcomeIndex], outcomeTokensToBuy, "");
-
         emit FPMMBuy(msg.sender, investmentAmount, feeAmount, outcomeIndex, outcomeTokensToBuy);
     }
-
     function sell(uint returnAmount, uint outcomeIndex, uint maxOutcomeTokensToSell) external {
         uint outcomeTokensToSell = calcSellAmount(returnAmount, outcomeIndex);
         require(outcomeTokensToSell <= maxOutcomeTokensToSell, "maximum sell amount exceeded");
-
         conditionalTokens.safeTransferFrom(msg.sender, address(this), getPositionIds()[outcomeIndex], outcomeTokensToSell, "");
-
         uint feeAmount = (returnAmount * fee) / (ONE - fee);
         feePoolWeight = feePoolWeight + feeAmount;
         uint returnAmountPlusFees = returnAmount + feeAmount;
         mergePositionsThroughAllConditions(returnAmountPlusFees);
-
         require(collateralToken.transfer(msg.sender, returnAmount), "return transfer failed");
-
         emit FPMMSell(msg.sender, returnAmount, feeAmount, outcomeIndex, outcomeTokensToSell);
     }
 }
-
-
 // for proxying purposes
 contract FixedProductMarketMakerData {
     mapping (address => uint256) internal _balances;
     mapping (address => mapping (address => uint256)) internal _allowances;
     uint256 internal _totalSupply;
-
-
     bytes4 internal constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
     mapping(bytes4 => bool) internal _supportedInterfaces;
-
-
     event FPMMFundingAdded(
         address indexed funder,
         uint[] amountsAdded,
@@ -486,7 +394,6 @@ contract FixedProductMarketMakerData {
     bytes32[] internal conditionIds;
     uint internal fee;
     uint internal feePoolWeight;
-
     uint[] internal outcomeSlotCounts;
     bytes32[][] internal collectionIds;
     uint[] internal positionIds;
--- FPMMfactory.sol
+++ FPMMfactory.sol
@@ -6,7 +6,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ConditionalTokens} from "./ConditionalTokens.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import {FixedProductMarketMaker} from "./FPMM.sol";
+import { FixedProductMarketMaker } from "./FPMM.sol";
 
 contract FixedProductMarketMakerFactory {
     ConditionalTokens private conditionalTokens;
@@ -15,15 +15,11 @@ contract FixedProductMarketMakerFactory {
     uint[] private positionIds;
     bytes32 private conditionId;
     address private oracle;
+
     address private admin;
-    address private calt;
-    address private cant;
-    mapping(bytes32 => address) public questionIdtoaddress;
-    mapping(bytes32 => bytes32) public questionIdtoConditionId;
-    mapping(bytes32 => mapping(uint => bytes32))
-        public questionIdtoCollectionId;
-    mapping(bytes32 => mapping(uint => uint))
-        public questionIdtoPositionId;
+    address private _collateralTokenAddress;
+    address private _conditionalTokensAddress;
+    mapping(bytes32 => address) public questionIdToFpmmAddress;
     event FixedProductMarketMakerCreation(
         address indexed creator,
         FixedProductMarketMaker newFactory,
@@ -37,117 +33,58 @@ contract FixedProductMarketMakerFactory {
         address _conditionalTokensAddr,
         address _collateralTokenAddr,
         address _oracle
-    ) public {
+    ) {
         conditionalTokens = ConditionalTokens(_conditionalTokensAddr);
         collateralToken = IERC20(_collateralTokenAddr);
         oracle = _oracle;
         admin = msg.sender;
-        calt = _collateralTokenAddr;
-        cant = _conditionalTokensAddr;
+        _collateralTokenAddress = _collateralTokenAddr;
+        _conditionalTokensAddress = _conditionalTokensAddr;
     }
 
     bytes32 parentCollectionId = bytes32(0);
-    modifier OnlyAdmin() {
+    modifier onlyAdmin() {
         require(msg.sender == admin, "only admin");
         _;
     }
 
     function createFixedProductMarketMaker(
-        string memory playername,
-        string memory playersymbol,
+        string memory lpTokenName,
+        string memory lpTokenSymbol,
         uint256 _fee,
         bytes32 _questionId
-    ) external OnlyAdmin returns (FixedProductMarketMaker) {
-
-
-        
- 
-        FixedProductMarketMaker newPlayer = new FixedProductMarketMaker(
-            playername,
-            playersymbol,
-            cant,
-            calt,
-            _fee,
-            oracle,
+    ) external onlyAdmin returns (address) {
+        FixedProductMarketMaker newFpmm = new FixedProductMarketMaker(
+            lpTokenName,
+            lpTokenSymbol,
+            _conditionalTokensAddress,
+            _collateralTokenAddress,
             _questionId,
-            admin
+            oracle,
+            _fee
         );
-
-
-        
-
-
-
-        conditionalTokens.prepareCondition(oracle,_questionId,2);
-
-        
-        conditionId = conditionalTokens.getConditionId(oracle,_questionId,2);
-        questionIdtoConditionId[_questionId] = conditionId;
-        collectionIds[0] = conditionalTokens.getCollectionId(
-            bytes32(0),
-            conditionId,
-            0
-        );
-        collectionIds[1] = conditionalTokens.getCollectionId(
-            bytes32(0),
-            conditionId,
-            1
-        );
-        questionIdtoCollectionId[_questionId][0] = collectionIds[0];
-        questionIdtoCollectionId[_questionId][1] = collectionIds[1];
-        positionIds[0] = conditionalTokens.getPositionId(
-            collateralToken,
-            collectionIds[0]
-        );
-        positionIds[1] = conditionalTokens.getPositionId(
-            collateralToken,
-            collectionIds[1]
-        );
-        questionIdtoPositionId[_questionId][0] = positionIds[0];
-        questionIdtoPositionId[_questionId][1] = positionIds[1];
 
         emit FixedProductMarketMakerCreation(
             msg.sender,
-            newPlayer,
+            newFpmm,
             conditionalTokens,
             collateralToken,
             conditionId,
             _fee
         );
-        questionIdtoaddress[_questionId] = address(newPlayer);
-        return newPlayer;
+        questionIdToFpmmAddress[_questionId] = address(newFpmm);
+
+        newFpmm.transferOwner(msg.sender);
+
+        return address(newFpmm);
     }
 
-    function getaddressbyquestionId(bytes32 _questionId)
+    function getAddressByQuestionId(bytes32 _questionId)
         public
         view
-      
         returns (address)
     {
-        return questionIdtoaddress[_questionId];
-    }
-
-    function getconditionIdByquestionId(bytes32 _questionId)
-        public
-        view
-
-        returns (bytes32)
-    {
-        return questionIdtoConditionId[_questionId];
-    }
-
-    function getcollectionIdByquestionId(
-        bytes32 _questionId,
-        uint256 outcomeIndex
-    ) public view returns (bytes32) {
-        return questionIdtoCollectionId[_questionId][outcomeIndex];
-    }
-
-    function getpositionIdByquestionId(
-        bytes32 _questionId,
-        uint256 outcomeIndex
-    ) public view  returns (uint) {
-        return questionIdtoPositionId[_questionId][outcomeIndex];
+        return questionIdToFpmmAddress[_questionId];
     }
 }
 
