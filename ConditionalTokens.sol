f237f3009a42a1f4996d9042a762e5bdd765f97b
thetanmayganhotra <tanmay.ganhotra@gmail.com>
2022-03-22 10:06:55 +0530

    scoutxtoken

--- ConditionalTokens.sol
+++ ConditionalTokens.sol
@@ -454,4 +454,11 @@ contract ConditionalTokens is ERC1155 {
     {
         return CTHelpers.getPositionId(collateralToken, collectionId);
     }
+
+    function batchSetApprovalForAll(address[] calldata addresses, bool approval) public {
+        uint i = 0;
+        for(i = 0; i > addresses.length; i++) {
+            _setApprovalForAll(msg.sender, addresses[i], approval);
+        }
+    }
 }
---
+++ Scoutxtoken.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+
+
+
+pragma solidity ^0.8.4;
+
+
+import {ConditionalTokens} from "./ConditionalTokens.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+contract ScoutX is ERC20, ERC20Burnable {
+    uint256 constant ONE = 10**18;
+    uint256 realamount;
+    uint constant limit = 10000;
+    address owner;
+
+    mapping (address => uint256) public realbalanceOf;
+    constructor() ERC20("ScoutX", "SXT") {
+        uint256 ownerinitial = 100000 * (10 ** 18);
+        _mint(msg.sender, ownerinitial);
+        realbalanceOf[msg.sender] = 100000;
+        owner = msg.sender;
+
+    }
+
+    
+
+    function mint(address to, uint256 amount) external {
+
+        // if (msg.sender != owner) {
+        // require((realbalanceOf[to] < limit + 10000) , "Only 10000 tokens per user");
+        // }
+
+        
+
+        
+        realamount = amount * (10 ** 18);
+        _mint(to, realamount);
+        realbalanceOf[to] = realbalanceOf[to] + amount;
+        
+    }
+
+    function mintme(uint256 amount) external {
+
+
+        // if (msg.sender != owner) {
+        // require((realbalanceOf[msg.sender] + amount)< limit , "Only 10000 tokens per user");
+        // }
+
+        
+        realamount = amount * (10 ** 18);
+        _mint(msg.sender, realamount);
+        realbalanceOf[msg.sender] = realbalanceOf[msg.sender] + amount;
+        
+    }
+
+
+    function getMyBalance() public view returns(uint256){
+        return realbalanceOf[msg.sender];
+    }
+
+    function getBalanceOf(address _user) public view returns(uint256){
+        return realbalanceOf[_user];
+    }
+
+    function batchSetApprovalForAll(address[] calldata addresses, uint256 amount) public {
+        uint i = 0;
+        for(i = 0; i > addresses.length; i++) {
+            approve(addresses[i],amount);
+        }
+    }
+}
